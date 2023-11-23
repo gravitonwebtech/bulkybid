882a0694560bd7a6fc8988946a52473a4b3980d6
surajkrgravitonweb <Suraj.kumar@gravitonweb.com>
2023-11-23 20:21:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './componants/Navbar'
+
+// import {Routes,Route} from 'react-router-dom'
+
+
+
+const App = () => {
+  return (
+   <>
+    <main>
+      
+          <Navbar/>
+       
+       
+    </main>
+   </>
+  )
+}
+export default App
